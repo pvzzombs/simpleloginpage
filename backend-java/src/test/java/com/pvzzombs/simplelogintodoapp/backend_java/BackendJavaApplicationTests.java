08e9f2fe4786d36d8aa376038b208020609b0d34
pvzzombs <pvzzombs@gmail.com>
2024-11-29 14:36:47 +0800

    Prepare java backend

---
+++ backend-java/src/test/java/com/pvzzombs/simplelogintodoapp/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvzzombs.simplelogintodoapp.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
